7480aa9dfb5d4aa22032357e7743a748cd44a284
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-19 10:04:21 +0200

    Add Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+import "./Navigation.css"
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul className="navigation">
+        <li className="navigation-links">
+          <NavLink to="/home">portfolio</NavLink>
+        </li>
+        <li className="navigation-links">
+          <NavLink to="/sobre-mi">sobre mí</NavLink>
+        </li>
+        <li className="navigation-links">
+          <NavLink to="/contacto">contacto</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation
