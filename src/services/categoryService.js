6295875dc2cb931ebb40a9cd324918a9502eb947
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-19 17:35:13 +0200

    Add categories fetch all

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -12,3 +12,13 @@ export const getCategoryById = async (categoryId) => {
     throw error;
   }
 };
+
+export const getAllCategories = async () => {
+  try {
+    const response = await axios.get("http://localhost:8080/api/v1/categorias");
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching all categories", error);
+    throw error;
+  }
+};
