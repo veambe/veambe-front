882b064845bf9277fd122a79715c66270db9eaaf
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-19 10:03:24 +0200

    Add Header component & styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+import Navigation from "../Navigation/Navigation";
+
+const Header = () => {
+  return (
+    <header>
+          <Link to={"/home"}>
+        <img
+          className="header-logo"
+          src="/images/logo-veambe.webp"
+          alt="logo veambe"
+          width="114"
+          height="38"
+        />
+      </Link>
+      <Navigation /> 
+      <a
+        className="insta-link"
+        href="https://www.instagram.com/veambe/?hl=es"
+        target="_blank"
+      >
+        instagram
+      </a>
+    </header>
+  );
+};
+
+export default Header;
