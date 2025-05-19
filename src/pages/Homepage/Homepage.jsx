65130456e9bed44623b48596c6cffe2da099de52
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-19 11:28:21 +0200

    Add homepage component & style

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,16 @@
+import "./Homepage.css"
+
+const Homepage = () => {
+
+  return (
+    <section className="homepage-main">
+      <img
+        className="hero"
+        src="/images/heroVeambe.png"
+        alt="logo veambe"
+      />
+    </section>
+  );
+}
+
+export default Homepage
