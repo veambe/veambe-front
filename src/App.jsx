15cda0ad515be9ee71b2953472c687201af71c7d
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-19 12:58:38 +0200

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
-
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
-
   return (
     <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
