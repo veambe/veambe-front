f80b8ca36c3dd9fded69b8e2d39085edcd68d2ad
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-21 16:54:02 +0200

    Add styles to category page

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,6 +1,7 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./CategoryPage.css"
 
 const CategoryDetail = () => {
   const { categoryName } = useParams();
@@ -22,7 +23,6 @@ const CategoryDetail = () => {
               const imageRes = await axios.get(
                 `http://localhost:8080/api/v1/images/obra/${art.id}`
               );
-              // añade la primera imagen que encuentre
               return {
                 ...art,
                 filePath: imageRes.data[0]?.filePath || null,
@@ -54,33 +54,40 @@ const CategoryDetail = () => {
   if (loading) return <p>Cargando obras de la categoría...</p>;
 
   return (
-    <div>
-      <h2>{decodeURIComponent(categoryName)}</h2>
+    <section>
+      <h1 className="category-page-title">
+        {decodeURIComponent(categoryName)}
+      </h1>
       {artworks.length === 0 ? (
         <p>No hay obras en esta categoría.</p>
       ) : (
-        <ul>
+        <ul className="category-page-list">
           {artworks.map((art) => (
             <li key={art.id}>
-              <h3>{art.title}</h3>
+              <Link
+                to={`/trabajo/obra/${encodeURIComponent(art.id)}`}
+                className="category-page-artwork"
+              >
+                <h3 className="artwork-title">{art.title}</h3>
 
-              {art.filePath ? (
-                <img
-                  src={`http://localhost:8080/${art.filePath.replace(
-                    /\\/g,
-                    "/"
-                  )}`}
-                  alt={art.title}
-                  style={{ width: "200px", height: "auto" }}
-                />
-              ) : (
-                <p>No image available</p>
-              )}
+                {art.filePath ? (
+                  <img
+                    src={`http://localhost:8080/${art.filePath.replace(
+                      /\\/g,
+                      "/"
+                    )}`}
+                    alt={art.title}
+                    style={{ width: "500px", height: "auto" }}
+                  />
+                ) : (
+                  <p>No hay imagen disponible</p>
+                )}
+              </Link>
             </li>
           ))}
         </ul>
       )}
-    </div>
+    </section>
   );
 };
 
