4329a2e77f054c617c4a4b8905ad6dacb93ff6e2
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-22 13:34:32 +0200

    Add styles to artwork list

--- src/components/ArtworkList/ArtworkList.jsx
+++ src/components/ArtworkList/ArtworkList.jsx
@@ -3,7 +3,9 @@ import {
   getAllArtworks,
   deleteArtworkById,
 } from "../../services/artworkService";
+import "./ArtworkList.css"
 import { Link } from "react-router-dom";
+
 
 const ArtworkListPage = () => {
   const [artworks, setArtworks] = useState([]);
@@ -40,22 +42,22 @@ const ArtworkListPage = () => {
 
   return (
     <div>
-      <h2>Lista de Obras</h2>
+      <h2 className="artwork-list-title">Gestiona tus obras</h2>
       {artworks.length === 0 ? (
         <p>No artworks available.</p>
       ) : (
-        <ul>
+        <ul className="artworklist-container">
           {artworks.map((art) => (
-            <li key={art.id}>
-              {art.title}{" "}
+            <li className="artwork-list" key={art.id}>
+              &#x2022;{art.title}{" "}
               <button
                 onClick={() => handleDelete(art.id)}
-                style={{ color: "red" }}
+                className="delete-button"
               >
                 Borrar
               </button>
               <Link to={`/admin/edit/${art.id}`}>
-                <button>Editar</button>
+                <button className="edit-button">Editar</button>
               </Link>
             </li>
           ))}
