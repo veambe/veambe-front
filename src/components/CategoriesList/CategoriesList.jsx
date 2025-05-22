f7793d7c8b1c3191c89e3b868561ad72d0fe68ab
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-22 12:35:03 +0200

    Fix categories list in homepage

--- src/components/CategoriesList/CategoriesList.jsx
+++ src/components/CategoriesList/CategoriesList.jsx
@@ -1,11 +1,33 @@
 import { useEffect, useState } from "react";
 import { getAllCategories } from "../../services/categoryService";
 import { Link } from "react-router-dom";
-import "./CategoriesList.css"
+import "./CategoriesList.css";
 
 const CategoriesList = () => {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const normalizeName = (name) => {
+    return name
+      .toLowerCase()
+      .normalize("NFD") 
+      .replace(/[\u0300-\u036f]/g, "") 
+      .replace(/\s+/g, ""); 
+  };
+
+
+  const categoryImages = {
+    murales: "https://i.ibb.co/fHg39hF/img-seccion-mural.png",
+    autoedicion: "https://i.ibb.co/PhNvcw1/img-seccion-autoedicion.png",
+    ceramica: "https://i.ibb.co/55Wwvry/img-seccion-ceramica.png",
+    grabado: "https://i.ibb.co/RCf4GT6/img-seccion-grabado.png",
+    ilustracionpublicitaria:
+      "https://i.ibb.co/k8BxWdT/img-seccion-ilu-publi.png",
+    obrapersonal: "https://i.ibb.co/dkwpjMJ/img-seccion-obra-personal.png",
+    productosilustrados:
+      "https://i.ibb.co/PxYtgWh/img-seccion-productos-ilustrados.png",
+    textil: "https://i.ibb.co/mD7yNWR/img-seccion-textil.png",
+  };
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -30,8 +52,14 @@ const CategoriesList = () => {
       {categories.map((category) => (
         <li className="category-image" key={category.id}>
           <Link to={`/trabajo/categoria/${encodeURIComponent(category.name)}`}>
-          
-            <img src="https://i.ibb.co/fHg39hF/img-seccion-mural.png" width={800} />
+            <img
+              src={
+                categoryImages[normalizeName(category.name)] ||
+                "https://via.placeholder.com/800"
+              }
+              width={800}
+              alt={category.name}
+            />
           </Link>
         </li>
       ))}
